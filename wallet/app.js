cf05f8b2600e8afb2e72c7858f74f35b285f2798
EDUARD <eduardsteveen@gmail.com>
2022-11-30 19:57:16 -0500

    se realiza la conecciona base de datos

--- wallet/app.js
+++ wallet/app.js
@@ -1,4 +1,5 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 
 const app = express()
@@ -7,6 +8,14 @@ const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log("El servidor está ejecutandose correctamente.");
+})
+
+mongoose.connect("mongodb+srv://eduard:eduard@cluster0.cz5cjtl.mongodb.net/inicio?retryWrites=true&w=majority", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+    console.log("se ha conectado la base de datos")
+    }
 })
 
 app.use(express.json())
